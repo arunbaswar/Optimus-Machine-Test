58c88aebf2d6d3d3c11d1f9b79ca9d3a7c040a2b
arunbaswar <arunbaswar@gmail.com>
2016-11-25 17:11:05 +0530

    uploaded required files
    optimus machine test code

---
+++ app/dashboard/dashboard.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'dashboard';
+    angular.module('app').controller(controllerId, ['common', 'datacontext', dashboard]);
+
+    function dashboard(common, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.news = {
+            title: 'Optimus Machine test',
+            description: 'Optimus Machine test is a SPA template.'
+        };
+        vm.messageCount = 0;
+        vm.people = [];
+        vm.title = 'Dashboard';
+
+        activate();
+
+        function activate() {
+            var promises = [getMessageCount(), getPeople()];
+            common.activateController(promises, controllerId)
+                .then(function () { log('Activated Optimus Machine Test View'); });
+        }
+
+        function getMessageCount() {
+            return datacontext.getMessageCount().then(function (data) {
+                return vm.messageCount = data;
+            });
+        }
+
+        function getPeople() {
+            return datacontext.getPeople().then(function (data) {
+                return vm.people = data;
+            });
+        }
+    }
+})();
